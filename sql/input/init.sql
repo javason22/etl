60ce75e5b448541498bc657404064d1a650b3a8f
Javalienca <122120390+javason22@users.noreply.github.com>
2024-05-27 10:38:29 -0700

    Put disable ONLY_FULL_GROUP_BY statement into database_read init.sql

--- sql/input/init.sql
+++ sql/input/init.sql
@@ -1,2 +1,6 @@
 -- sql/init.sql
 CREATE DATABASE IF NOT EXISTS database_read;
+USE database_read;
+-- disable ONLY_FULL_GROUP_BY
+SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+SET PERSIST sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
