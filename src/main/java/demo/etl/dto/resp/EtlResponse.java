be34f6c30d41cb4e3873e0c53080d678c5159a1f
Javalienca <122120390+javason22@users.noreply.github.com>
2024-05-27 10:33:08 -0700

    Fix a warning during compilation
    The warning is due to the EtlResponse generates same hashCode as GeneralResponse

--- src/main/java/demo/etl/dto/resp/EtlResponse.java
+++ src/main/java/demo/etl/dto/resp/EtlResponse.java
@@ -2,16 +2,19 @@ package demo.etl.dto.resp;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 @Schema(name = "EtlResponse", description = "Response object for ETL trigger")
 public class EtlResponse extends GeneralResponse{
     @Schema(description = "List of wager summaries")
     List<WagerSummaryResponse> wagerSummaries;
+
     public EtlResponse(String status, String message, List<WagerSummaryResponse> wagerSummaries) {
         super(status, message);
         this.wagerSummaries = wagerSummaries;
